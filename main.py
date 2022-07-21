a1c7ac073076ce23c5541a462635d4420d098b7e
Sehyun Kim <cloudest@dgist.ac.kr>
2022-07-21 11:04:44 +0000

    Increased the number of caption to 5

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 bert_model = BertModel.from_pretrained("bert-base-uncased")
 translator = Translator()
 cos_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
+nCaption = 5
+
+imgPath = os.path.join(abs_path, 'static')
+imgPath = os.path.join(imgPath, 'instagram_img')
 
 with open("bg_text_feat.pkl", "rb") as fd:
     bg_text_feat = pickle.load(fd)
@@ -60,30 +64,43 @@ def upload():
         # Begin step 1) generating a caption from uploaded image step 2) recommending background asset
         begin_time = time.time()
         # img2text_CLIP takes an image file(path) and returns a caption(text) that describes input image the best
-        caption_orig = img2text_CLIP(filePathList[0])
+        caption_orig_list = img2text_CLIP(filePathList[0])
+        rec_img_fName_list = []
         
-        encoded_input = tokenizer(caption_orig, return_tensors='pt')
-        caption_feat = bert_model(**encoded_input).pooler_output
+        for i in range(nCaption):
+            encoded_input = tokenizer(caption_orig_list[i], return_tensors='pt')
+            caption_feat = bert_model(**encoded_input).pooler_output
+
+            sim_score_list = []
+            for bg_img_fName, candidate in bg_text_feat.items():
+                sim_score_list.append((cos_similarity(caption_feat, candidate), bg_img_fName))
+            rec_img_fName = max(sim_score_list)[1]
+            rec_img_fName_list.append(rec_img_fName)
+        
+        # sorted_score_list = sorted(sim_score_list)
+        # rec_img_fName = sorted_score_list[0][1]
+        
+        rel_img_path_list = []
+        for i in range(nCaption):
+            # rec_img_fName = '000101' imgPath = 'static/instagram_img/'
+            rec_img_fPath = os.path.join(imgPath, rec_img_fName_list[i])
+            # if img file extension is jpg
+            if os.path.isfile(rec_img_fPath + '.jpg'):
+                fName_with_ext = rec_img_fName_list[i] + '.jpg'
+            # if img file extension is png
+            elif os.path.isfile(rec_img_fPath + '.png'):
+                fName_with_ext = rec_img_fName_list[i] + '.png'
+                
+            rel_img_path_list.append(os.path.join('instagram_img', fName_with_ext) )
+        
+        caption_orig_best = caption_orig_list[0]
+        caption_trans_best = translator.translate(caption_orig_best, src='en', dest='ko').text
         
         end_time = time.time()
-        app.logger.info(end_time - begin_time)
+        exec_time = end_time - begin_time
         
-        caption_trans = translator.translate(caption_orig, src='en', dest='ko').text
-        
-        sim_score_list = []
-
-        for bg_img_fName, candidate in bg_text_feat.items():
-            sim_score_list.append((cos_similarity(caption_feat, candidate), bg_img_fName))
-        
-        # with open("sim_score_dict", "wb") as listfp:
-        #     pickle.dump(sim_score_dict, listfp)
-        sorted_score_list = sorted(sim_score_list)
-        rec_img_fName = sorted_score_list[0][1]
-        
-        exec_time = begin_time - end_time
-        
-        return render_template('result.html', filePath=relFilePathList[0], caption_eng=caption_orig,
-                               caption_ko=caption_trans, rec_img_file=rec_img_fName, time=round(exec_time, 2))
+        return render_template('result.html', num_caption=nCaption, filePath=relFilePathList[0], caption_eng=caption_orig_best,
+                               caption_ko=caption_trans_best, recommended_imgs=rel_img_path_list, time=round(exec_time, 2))
 
     except Exception as e:
         print(e)
